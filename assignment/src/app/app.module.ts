513e1c9aedd4f31a3c16a9b7fac2862626f4d1bf
nhan2231 <nhan2269@yahoo.com.vn>
2019-10-15 21:26:20 +0700

    5th

--- assignment/src/app/app.module.ts
+++ assignment/src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgxPaginationModule } from 'ngx-pagination'
+import { FormsModule } from '@angular/forms'
+import { AngularFireModule } from '@angular/fire'
+import { AngularFirestore } from '@angular/fire/firestore'
 
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
@@ -15,6 +19,16 @@ import { DangNhapComponent } from './dang-nhap/dang-nhap.component';
 import { DangKyComponent } from './dang-ky/dang-ky.component';
 import { CapNhatComponent } from './cap-nhat/cap-nhat.component';
 import { DoiPassComponent } from './doi-pass/doi-pass.component';
+
+var config = {
+  apiKey: "AIzaSyBGVTvHx6YfHbOJEzGFRS20G6COZlkt4tc",
+  authDomain: "angular-porfolio-3aa73.firebaseapp.com",
+  databaseURL: "https://angular-porfolio-3aa73.firebaseio.com",
+  projectId: "angular-porfolio-3aa73",
+  storageBucket: "angular-porfolio-3aa73.appspot.com",
+  messagingSenderId: "15240368631",
+  appId: "1:15240368631:web:0094c3eca62210dc55def2"
+};
 
 @NgModule({
   declarations: [
@@ -33,19 +47,23 @@ import { DoiPassComponent } from './doi-pass/doi-pass.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     NgbModule,
+    NgxPaginationModule,
+    AngularFireModule,
+    AngularFirestore,
     RouterModule.forRoot([
-      {path: '', component: IndexComponent},
-      {path: 'gioiThieu', component:GioiThieuComponent},
-      {path: 'lienHe', component:LienHeComponent},
-      {path: 'hoiDap', component:HoiDapComponent},
-      {path: 'danhMuc', component:DanhMucComponent},
-      {path: 'tracNghiem', component:TracNghiemComponent},
-      {path: 'gopY', component:GopYComponent},
-      {path: 'dangKy', component:DangKyComponent},
-      {path: 'dangNhap', component:DangNhapComponent},
-      {path: 'doiPass', component:DoiPassComponent},
-      {path: 'capNhat', component:CapNhatComponent},
+      { path: '', component: IndexComponent },
+      { path: 'gioiThieu', component: GioiThieuComponent },
+      { path: 'lienHe', component: LienHeComponent },
+      { path: 'hoiDap', component: HoiDapComponent },
+      { path: 'danhMuc', component: DanhMucComponent },
+      { path: 'tracNghiem', component: TracNghiemComponent },
+      { path: 'gopY', component: GopYComponent },
+      { path: 'dangKy', component: DangKyComponent },
+      { path: 'dangNhap', component: DangNhapComponent },
+      { path: 'doiPass', component: DoiPassComponent },
+      { path: 'capNhat', component: CapNhatComponent },
       // {path:'**', redirectTo:'product', pathMatch:'full'},
     ])
   ],
